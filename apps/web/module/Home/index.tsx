bd148a16615b30b055a529fcc82409a00cf118ad
Dyayy026 <arjayandal93@gmail.com>
2024-01-24 00:31:44 -0800

    task(web): Added custom hook useGetAllUsers

--- apps/web/module/Home/index.tsx
+++ apps/web/module/Home/index.tsx
@@ -4,7 +4,6 @@ import useGetAllUsers from "@/common/hooks/useGetAllUser";
 import React from "react";
 
 const ModuleHome = () => {
-  const { data } = useGetAllUsers();
   return <div></div>;
 };
 
