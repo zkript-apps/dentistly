1dd0e5c1dbf5aa49c5c556b02d9fa744f54399dd
Dyayy026 <Dyayy026@users.noreply.github.com>
2024-01-25 05:49:05 +0000

    Prettified Code!

--- apps/web/common/hooks/useUpdateUser.ts
+++ apps/web/common/hooks/useUpdateUser.ts
@@ -1,20 +1,17 @@
-import { ApiService } from "@/common/service/api"
+import { ApiService } from "@/common/service/api";
 import { IUser } from "../types";
-import { useMutation } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query";
 import { API_URL_USER } from "../constants/api";
 
-export async function updateUser(
-  id: string | null,
-  props: IUser
-) {
-  const apiService = new ApiService()
-  return await apiService.patch(`${API_URL_USER}/${id}`,props)
+export async function updateUser(id: string | null, props: IUser) {
+  const apiService = new ApiService();
+  return await apiService.patch(`${API_URL_USER}/${id}`, props);
 }
 
 function useUpdateUser(id: string | null) {
   const query = useMutation({
     mutationFn: (props: IUser) => updateUser(id, props),
-  })
-  return query
+  });
+  return query;
 }
 export default useUpdateUser;
