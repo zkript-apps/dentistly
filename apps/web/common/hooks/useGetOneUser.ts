e57f46eb500568b4348fec1fe7661ca6dacbe2a8
Dyayy026 <arjayandal93@gmail.com>
2024-01-24 18:59:18 -0800

    task(web): Added custom hook useGetOneUser

---
+++ apps/web/common/hooks/useGetOneUser.ts
@@ -0,0 +1,19 @@
+import { ApiService } from "@/common/service/api"
+import { API_URL_USER } from "../constants/api";
+import { useQuery } from "@tanstack/react-query"
+
+export async function getOneUserId(id: string | undefined) {
+  const apiService = new ApiService()
+  return await apiService.get(`${API_URL_USER}/${id}`)
+}
+
+function useGetOneUser(id: string | undefined) {
+  const query = useQuery({
+    queryKey: ["users", id],
+    queryFn: () => getOneUserId(id),
+    refetchOnWindowFocus: false,
+    enabled: !!id,
+  })
+  return query
+}
+export default useGetOneUser;
--- apps/web/module/Home/index.tsx
+++ apps/web/module/Home/index.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import useGetOneUser from "@/common/hooks/useGetOneUser";
 import React from "react";
 
 const ModuleHome = () => {
+  const {data} = useGetOneUser("65b0b8ffe165a66116a3c16d")
   return <div></div>;
 };
 
