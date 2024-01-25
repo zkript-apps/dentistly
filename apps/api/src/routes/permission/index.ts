17591edcc83a9fa1df3620c923954353f0928957
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-01-25 14:55:37 +0800

    task(api): Created delete permission endpoint

--- apps/api/src/routes/permission/index.ts
+++ apps/api/src/routes/permission/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllPermission, getPermission, updatePermission } from "./default";
+import { deletePermission, getAllPermission, getPermission, updatePermission } from "./default";
 
 const router = express.Router()
 router.get('/', getAllPermission)
 router.get('/:id', getPermission)
 router.patch('/:id', updatePermission)
+router.delete('/:id', deletePermission)
 
 export default router
