6e88e7646bcca465cdc449854a943cbef6af0c18
Dyayy026 <arjayandal93@gmail.com>
2024-01-24 19:04:02 -0800

    task(web): Added custom hook useGetOneUser

--- apps/web/module/Home/index.tsx
+++ apps/web/module/Home/index.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import useGetOneUser from "@/common/hooks/useGetOneUser";
 import React from "react";
 
 const ModuleHome = () => {
-  const { data } = useGetOneUser("65b0b8ffe165a66116a3c16d");
   return <div></div>;
 };
 
