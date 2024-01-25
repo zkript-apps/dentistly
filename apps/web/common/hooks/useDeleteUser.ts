402073725e5ac06c63deaa4a463262fbff35a2c0
Dyayy026 <Dyayy026@users.noreply.github.com>
2024-01-25 07:06:31 +0000

    Prettified Code!

--- apps/web/common/hooks/useDeleteUser.ts
+++ apps/web/common/hooks/useDeleteUser.ts
@@ -1,18 +1,16 @@
-import { ApiService } from "@/common/service/api"
+import { ApiService } from "@/common/service/api";
 import { API_URL_USER } from "../constants/api";
-import { useMutation } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query";
 
 export async function removeUser(id: string) {
-  const apiService = new ApiService()
-  return await apiService.delete(
-    `${API_URL_USER}/${id}`
-  )
+  const apiService = new ApiService();
+  return await apiService.delete(`${API_URL_USER}/${id}`);
 }
 
 function useDeleteUser(id: string) {
   const query = useMutation({
     mutationFn: () => removeUser(id),
-  })
-  return query
+  });
+  return query;
 }
 export default useDeleteUser;
