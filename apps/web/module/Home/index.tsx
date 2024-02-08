9ebc74dcb12356b314340ccf31b5653f0e417957
jeusnsn <jeusdsn@gmail.com>
2024-02-08 08:13:04 +0800

    feat: Reusable Button

--- apps/web/module/Home/index.tsx
+++ apps/web/module/Home/index.tsx
@@ -1,7 +1,40 @@
+import { Button } from "@/common/components/ui/Button";
 import React from "react";
 
 const ModuleHome = () => {
-  return <div>Home Module</div>;
+  return <div>
+    {/* <button className="bg-primary-500">Click me </button> */}
+    <Button>
+      Default
+    </Button>
+    <Button variant={"primary"}>
+      Primary
+    </Button>
+    <Button variant={"danger"}>
+      Danger
+    </Button>
+    <Button variant={"warning"}>
+      Warning
+    </Button>
+    <Button variant={"success"}>
+      Success
+    </Button>
+    <Button variant={"outline"}>
+      Outline
+    </Button>
+    <Button variant={"secondary"}>
+      Secondary
+    </Button>
+    <Button variant={"ghost"}>
+      Ghost
+    </Button>
+    <Button variant={"link"}>
+      Link
+    </Button>
+    <Button variant={"shaded"}>
+      Shaded
+    </Button>
+  </div>;
 };
 
 export default ModuleHome;
