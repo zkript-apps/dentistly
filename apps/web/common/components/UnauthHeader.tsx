dbc429b346f42a1d7cb54374e3c0813fa6c2d338
nxo-al <renzo09357813687@gmail.com>
2024-02-08 11:37:15 +0800

    Created Reusable header for unauthenticated user

--- apps/web/common/components/UnauthHeader.tsx
+++ apps/web/common/components/UnauthHeader.tsx
@@ -1,26 +1,39 @@
 "use client";
-import { useState } from "react";
-
-const navigation = [
-	{ name: "Product", href: "#" },
-	{ name: "Features", href: "#" },
-	{ name: "Marketplace", href: "#" },
-	{ name: "Company", href: "#" },
-];
+import { Settings, CircleUser, Menu, Bell } from "lucide-react";
 
 export default function UnauthHeader() {
-	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
 	return (
-		<header className="bg-white">
+		<header className="bg-white border border-b shadow-sm pt-1 pb-1">
 			<nav
-				className="mx-auto flex w-full m-0 pl-3 pt-4 items-center justify-between lg:px-8"
+				className="mx-auto flex w-full m-0 items-center justify-evenly lg:px-8"
 				aria-label="Global"
 			>
+				{/* left part */}
+				<div className="p-5">
+					<Menu strokeWidth={1.25} />
+				</div>
 				<div className="flex lg:flex-1 font-bold">
 					<a href="#" className="">
 						Dentistly
 					</a>
+				</div>
+
+				{/* right part */}
+				<div className="flex justify-evenly w-auto items-center">
+					<div className="settings p-2">
+						<Settings strokeWidth={1.25} />
+					</div>
+					<div className="notif p-2">
+						<Bell strokeWidth={1.25} />
+					</div>
+					<div className="outside-border ml-2 mr-2 border border-dotted p-0.25 rounded-md border-gray-700">
+						<div className="create-btn  border-2 pb-2 pt-2 pl-3 pr-3 rounded-md bg-purple-600 text-white">
+							Create Organization
+						</div>
+					</div>
+					<div className="user-settings p-2">
+						<CircleUser strokeWidth={1.25} />
+					</div>
 				</div>
 			</nav>
 		</header>
