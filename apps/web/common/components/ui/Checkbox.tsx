83889c554a24106c78f7aac8a9685b4a633163c2
jeusnsn <jeusnsn@users.noreply.github.com>
2024-02-09 03:10:03 +0000

    Prettified Code!

--- apps/web/common/components/ui/Checkbox.tsx
+++ apps/web/common/components/ui/Checkbox.tsx
@@ -2,56 +2,45 @@ import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/common/helpers/cn";
 
-const checkBoxVariants = cva(
-  "rounded",
-  {
-    variants: {
-      variant:{
-          default:"w-4 h-4 bg-gray-100 border-gray-300",
-      },
-      size: {
-        default: "w-4 h-4",
-        sm: "w-4 h-4",
-        md: "w-6 h-6",
-      },
+const checkBoxVariants = cva("rounded", {
+  variants: {
+    variant: {
+      default: "w-4 h-4 bg-gray-100 border-gray-300",
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      default: "w-4 h-4",
+      sm: "w-4 h-4",
+      md: "w-6 h-6",
     },
   },
-);
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
 
 export interface checkBoxProps
   extends React.HTMLAttributes<HTMLElement>,
     VariantProps<typeof checkBoxVariants> {
-      checkBoxValue: string,
-      isChecked: boolean,
+  checkBoxValue: string;
+  isChecked: boolean;
 }
 
 const CheckBox = React.forwardRef<HTMLElement, checkBoxProps>(
-  (
-    {
-      className,
-      size,
-      children,
-      checkBoxValue,
-      variant,
-      ...props
-    },
-  ) => {
-    
+  ({ className, size, children, checkBoxValue, variant, ...props }) => {
     return (
       <div>
-      <input
-        type="checkbox"
-        id="default-checkbox"
-        className={cn(checkBoxVariants({variant, size, className}))}
-        {...props}
-      >
-       {children}
-      </input>
-      <label htmlFor="default-checkbox" className="ms-2 text-sm text-gray-90">{checkBoxValue}</label>
+        <input
+          type="checkbox"
+          id="default-checkbox"
+          className={cn(checkBoxVariants({ variant, size, className }))}
+          {...props}
+        >
+          {children}
+        </input>
+        <label htmlFor="default-checkbox" className="ms-2 text-sm text-gray-90">
+          {checkBoxValue}
+        </label>
       </div>
     );
   },
