404ab4936a523b7834b64ccfea536c7b9450d241
jeusnsn <jeusdsn@gmail.com>
2024-02-12 14:52:44 +0800

    feat: Reusable header for unauth user

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,8 @@
 import Sidebar from "@/common/components/Sidebar";
-import ModuleHome from "@/module/Home";
 import React from "react";
 
 const DefaultPage = async () => {
-  return <ModuleHome />;
+  return <Sidebar />;
 };
 
 export default DefaultPage;
