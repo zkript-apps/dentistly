ee0f3398c12216cf074e34bd34880dba2dcbe827
jeusnsn <jeusdsn@gmail.com>
2024-02-15 10:12:35 +0800

    on process

--- apps/web/app/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import Login from "@/app/login/page";
-import React from "react";
-
-const ModuleHome = () => {
-  return <Login/>;
-};
-
-export default ModuleHome;
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Login from "./login/page";
 
 const DefaultPage = async () => {
-  return <div> <Login/></div>;
+  return <div></div>;
 };
 
 export default DefaultPage;
