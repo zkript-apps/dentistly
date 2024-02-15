d5cdcc13592a593b01709165a7b9ae6cdbdd7e87
jeusnsn <jeusdsn@gmail.com>
2024-02-15 09:20:29 +0800

    feat: Login UI

--- apps/web/module/Home/index.tsx
+++ apps/web/module/Home/index.tsx
@@ -1,8 +1,7 @@
-import Login from "@/app/login/page";
 import React from "react";
 
 const ModuleHome = () => {
-  return <div><Login/>Home Module</div>;
+  return <div></div>;
 };
 
 export default ModuleHome;
