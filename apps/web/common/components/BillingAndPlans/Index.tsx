f7be9848f6ccc05d4e2a889d4fa9d22fc8e33ff8
Renzo <103110736+nxo-al@users.noreply.github.com>
2024-02-23 15:10:46 +0800

    Update Index.tsx

--- apps/web/common/components/BillingAndPlans/Index.tsx
+++ apps/web/common/components/BillingAndPlans/Index.tsx
@@ -45,7 +45,7 @@ function classNames(...classes: string[]) {
 }
 
 const Index = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [setIsModalOpen] = useState(false);
   const [selected, setSelected] = useState(settings[0]);
   const cancelButtonRef = useRef(null);
 
@@ -68,6 +68,7 @@ const Index = () => {
           <button onClick={openModal} className="underline text-blue-500 pr-1">
             Add a payment method
           </button>
+          
           so you can purchase premium images and vectors for ₱50 a piece.
         </div>
         <div className="pt-4">
@@ -197,6 +198,7 @@ const Index = () => {
             <div className="flex items-center font-semibold text-xs pt-3 pb-2">
               Tax ID
               <span className="pl-1 h-full">
+                {' '}
                 <Info size={14} strokeWidth={3} />
               </span>
             </div>
@@ -326,9 +328,9 @@ const Index = () => {
                                       viewBox="0 0 24 24"
                                       fill="none"
                                       stroke="currentColor"
-                                      stroke-width="1.25"
-                                      stroke-linecap="round"
-                                      stroke-linejoin="round"
+                                      strokeWidth="1.25"
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
                                       className="lucide lucide-credit-card flex justify-start items-center w-auto pr-1"
                                       style={{ paddingLeft: "8px" }}
                                     >
