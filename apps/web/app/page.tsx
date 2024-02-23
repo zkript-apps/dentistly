a00bb3aaa86b53f76a64d92ecb278002da0dbb05
Renzo <103110736+nxo-al@users.noreply.github.com>
2024-02-23 14:05:33 +0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,4 @@
-import TeamBillingInfo from "@/common/components/BillingAndPlans/TeamBillingInfo";
-import PaymentMethod from "../common/components/BillingAndPlans/PaymentMethod";
-import TeamSubscriptions from "../common/components/BillingAndPlans/TeamSubscriptions";
-import TaxID from "@/common/components/BillingAndPlans/TaxID";
-import Team from "@/common/components/BillingAndPlans/Team";
-import PaymentMethodModal from "@/common/components/BillingAndPlans/PaymentMethodModal";
-import Index from "@/common/components/BillingAndPlans/Index";
-import ButtonToModal from "@/common/components/BillingAndPlans/Test";
+import Index from "@/common/components/BillingAndPlans/Index"
 
 const DefaultPage = async () => {
   return <Index />;
