009843a8c7d8a3b3380018bc058b81be6e74ef10
nxo-al <nxo-al@users.noreply.github.com>
2024-03-13 07:24:46 +0000

    Prettified Code!

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -7,14 +7,14 @@ import "./common/utils/mongodb";
 const app: Application = express();
 app.use(express.json());
 app.use(
-	cors({
-		origin: origins,
-		credentials: true,
-	})
+  cors({
+    origin: origins,
+    credentials: true,
+  }),
 );
 
 routes(app);
 
 app.listen(port, () => {
-	console.log(`API Server is running at ${port}`);
+  console.log(`API Server is running at ${port}`);
 });
