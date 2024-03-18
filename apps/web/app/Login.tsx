44ea9e5e39b8474c88c4204de5c97637da8485f1
nxo-al <nxo-al@users.noreply.github.com>
2024-03-18 03:53:36 +0000

    Prettified Code!

--- apps/web/app/Login.tsx
+++ apps/web/app/Login.tsx
@@ -3,33 +3,33 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import { UserCard } from "./userCard";
 
 export default function Login() {
-	const { data: session } = useSession();
-	console.log(session);
+  const { data: session } = useSession();
+  console.log(session);
 
-	if (session) {
-		return (
-			<>
-				<button
-					onClick={() => signOut()}
-					type="button"
-					className="btn btn-primary"
-				>
-					Sign Out of Google
-				</button>
-				<UserCard user={session?.user} />
-			</>
-		);
-	} else {
-		return (
-			<div>
-				<button
-					onClick={() => signIn()}
-					type="button"
-					className="btn btn-primary"
-				>
-					Sign In with Google
-				</button>
-			</div>
-		);
-	}
+  if (session) {
+    return (
+      <>
+        <button
+          onClick={() => signOut()}
+          type="button"
+          className="btn btn-primary"
+        >
+          Sign Out of Google
+        </button>
+        <UserCard user={session?.user} />
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <button
+          onClick={() => signIn()}
+          type="button"
+          className="btn btn-primary"
+        >
+          Sign In with Google
+        </button>
+      </div>
+    );
+  }
 }
--- apps/web/pages/api/auth/[...nextauth].ts
+++ apps/web/pages/api/auth/[...nextauth].ts
@@ -4,17 +4,17 @@ import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 
 const options = {
-	providers: [
-		FacebookProvider({
-			clientId: "1027899781769544",
-			clientSecret: "6f0084d9f7e4d22ec3eb4842bc5d553c",
-		}),
-		GoogleProvider({
-			clientId:
-				"513636772773-htf4ahh9upo5r4c3nvu9jkqdll5vf30n.apps.googleusercontent.com",
-			clientSecret: "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",
-		}),
-	],
+  providers: [
+    FacebookProvider({
+      clientId: "1027899781769544",
+      clientSecret: "6f0084d9f7e4d22ec3eb4842bc5d553c",
+    }),
+    GoogleProvider({
+      clientId:
+        "513636772773-htf4ahh9upo5r4c3nvu9jkqdll5vf30n.apps.googleusercontent.com",
+      clientSecret: "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",
+    }),
+  ],
 };
 
 export default NextAuth(options);
