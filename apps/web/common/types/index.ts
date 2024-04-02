8d65346f79c128408c00b2c05e844bf84d04decc
MarkDacsz <MarkDacsz@users.noreply.github.com>
2024-04-02 03:42:37 +0000

    Prettified Code!

--- apps/web/common/types/index.ts
+++ apps/web/common/types/index.ts
@@ -1,12 +1,12 @@
 export interface IUser {
-  id?: number
-  role?: any
-  registrationType?: any
+  id?: number;
+  role?: any;
+  registrationType?: any;
   email: string;
-  address?: string
-  password?: string
-  conatctNumber?: number
-  birthDate?: string
+  address?: string;
+  password?: string;
+  conatctNumber?: number;
+  birthDate?: string;
   createdAt?: string;
   deletedAt?: string;
   updatedAt?: string;
