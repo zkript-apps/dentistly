08cd68b4f8c57d5bd6505f5b0ea98cd9a6d1de5d
Vonn Joebert B. Achoy <vjachoy@gmail.com>
2024-04-04 16:24:19 +0800

    feat: update patient hook

--- apps/web/module/Patient/hooks/useUpdatePatient.ts
+++ apps/web/module/Patient/hooks/useUpdatePatient.ts
@@ -1,16 +1,16 @@
-import { API_URL_USER } from "@/common/constants/api";
+import { API_URL_PATIENT } from "@/common/constants/api";
 import { useMutation } from "@tanstack/react-query";
 import { ApiService } from "@/common/services/api";
 import { IPatient } from "@/common/types";
 
-export async function updatePatient(props: IPatient) {
+export async function updatePatient(props: IPatient, patientId: string) {
   const apiService = new ApiService();
-  return await apiService.post(`${API_URL_USER}/auth/register`, props);
+  return await apiService.patch(`${API_URL_PATIENT}/${patientId}`, props);
 }
 
-function useUpdatePatient() {
+function useUpdatePatient(patientId: string) {
   const query = useMutation({
-    mutationFn: (props: IPatient) => updatePatient(props),
+    mutationFn: (props: IPatient) => updatePatient(props, patientId),
   });
   return query;
 }
