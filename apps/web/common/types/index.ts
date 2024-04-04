eb2263c50231e0a6d95ce61702400ff5bb57304b
vjachoy17 <vjachoy17@users.noreply.github.com>
2024-04-04 07:43:56 +0000

    Prettified Code!

--- apps/web/common/types/index.ts
+++ apps/web/common/types/index.ts
@@ -1,4 +1,3 @@
-
 export interface IUserLogin {
   email: string;
   password: string;
--- apps/web/module/Patient/hooks/useDeletePatient.ts
+++ apps/web/module/Patient/hooks/useDeletePatient.ts
@@ -2,9 +2,9 @@ import { API_URL_PATIENT } from "@/common/constants/api";
 import { useMutation } from "@tanstack/react-query";
 import { ApiService } from "@/common/services/api";
 
-export async function patientUser (patientId: string) {
+export async function patientUser(patientId: string) {
   const apiService = new ApiService();
-  return await apiService.patch(`${API_URL_PATIENT}/${patientId}` );
+  return await apiService.patch(`${API_URL_PATIENT}/${patientId}`);
 }
 function useDeletePatient() {
   const query = useMutation({
