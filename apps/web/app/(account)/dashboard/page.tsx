e94f4dd7ecb1106274d37c7b2169a252dbe9437d
nxo-al <renzo09357813687@gmail.com>
2024-04-08 16:00:16 +0800

    Implement AuthGuard

--- apps/web/app/(account)/dashboard/page.tsx
+++ apps/web/app/(account)/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import AuthGuard from "@/common/components/AuthGuard";
 import Dashboard from "@/module/Dashboard";
 
 const DashboardPage = async () => {
-  return <Dashboard />;
+	return (
+		<AuthGuard>
+			<Dashboard />
+		</AuthGuard>
+	);
 };
 
 export default DashboardPage;
