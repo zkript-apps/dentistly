85ca5123686182f7e8e11b94c0b90425b64e0a64
nxo-al <nxo-al@users.noreply.github.com>
2024-04-09 07:18:40 +0000

    Prettified Code!

--- apps/web/module/People/index.tsx
+++ apps/web/module/People/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Peoples from "./components/People";
 
 const People = () => {
-	return (
-		<div>
-			<Peoples />
-		</div>
-	);
+  return (
+    <div>
+      <Peoples />
+    </div>
+  );
 };
 
 export default People;
