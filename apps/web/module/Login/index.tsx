e6e276220be7b470fd4dd93a6062c4424a336478
nxo-al <nxo-al@users.noreply.github.com>
2024-04-16 09:39:56 +0000

    Prettified Code!

--- apps/web/module/Login/index.tsx
+++ apps/web/module/Login/index.tsx
@@ -8,100 +8,100 @@ import { toast } from "sonner"; // Assuming `sonner` has a `toast` function for 
 import { IUserLogin } from "@/common/types";
 import { Button } from "@/common/components/shadcn/ui/button";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/common/components/shadcn/ui/card";
 import { Input } from "@/common/components/shadcn/ui/input";
 import { Label } from "@/common/components/shadcn/ui/label";
 
 const Login = () => {
-	const router = useRouter();
-	const { mutate: loginUser, isPending: isLoginPending } = useLogin();
-	const { register, handleSubmit } = useForm<IUserLogin>();
+  const router = useRouter();
+  const { mutate: loginUser, isPending: isLoginPending } = useLogin();
+  const { register, handleSubmit } = useForm<IUserLogin>();
 
-	const onSubmit = async (formData: IUserLogin) => {
-		try {
-			const callBackReq = {
-				onSuccess: (data: any) => {
-					if (!data.error && !isLoginPending) {
-						if (data.action?.data.action.link) {
-							router.push(data.action.link);
-						}
-					} else {
-						toast.error(String(data.message));
-					}
-				},
-				onError: (err: any) => {
-					toast.error(String(err));
-				},
-			};
+  const onSubmit = async (formData: IUserLogin) => {
+    try {
+      const callBackReq = {
+        onSuccess: (data: any) => {
+          if (!data.error && !isLoginPending) {
+            if (data.action?.data.action.link) {
+              router.push(data.action.link);
+            }
+          } else {
+            toast.error(String(data.message));
+          }
+        },
+        onError: (err: any) => {
+          toast.error(String(err));
+        },
+      };
 
-			loginUser(formData, callBackReq);
-		} catch (error) {
-			console.error("Error adding user:", error);
-			toast.error("An error occurred. Please try again later.");
-		}
-	};
+      loginUser(formData, callBackReq);
+    } catch (error) {
+      console.error("Error adding user:", error);
+      toast.error("An error occurred. Please try again later.");
+    }
+  };
 
-	return (
-		<form
-			onSubmit={handleSubmit(onSubmit)}
-			className="space-y-4"
-			action="#"
-			method="POST"
-		>
-			<div className="flex justify-center items-center h-screen">
-				<Card className="w-full max-w-sm">
-					<CardHeader>
-						<CardTitle className="text-2xl">Login</CardTitle>
-						<CardDescription>
-							Enter your email below to login to your account.
-						</CardDescription>
-					</CardHeader>
-					<CardContent className="grid gap-4">
-						<div className="grid gap-2">
-							<Label htmlFor="email">Email</Label>
-							<Input
-								{...register("email")}
-								type="email"
-								placeholder="m@example.com"
-								required
-							/>
-						</div>
-						<div className="grid gap-2">
-							<div className="flex items-center">
-								<Label htmlFor="password">Password</Label>
-								<Link
-									href="/forgot-password"
-									className="ml-auto inline-block text-sm underline"
-								>
-									Forgot your password?
-								</Link>
-							</div>
-							<Input {...register("password")} type="password" required />
-						</div>
-					</CardContent>
-					<CardFooter>
-						<div className="flex flex-col w-full items-center">
-							<Button variant="default" className="w-full" type="submit">
-								Sign in
-							</Button>
-							<div className="border w-auto text-center text-sm m-4">
-								Don&apos;t have an account?{" "}
-								<Link href="/create-account" className="underline">
-									Sign up
-								</Link>
-							</div>
-						</div>
-					</CardFooter>
-				</Card>
-			</div>
-		</form>
-	);
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="space-y-4"
+      action="#"
+      method="POST"
+    >
+      <div className="flex justify-center items-center h-screen">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                {...register("email")}
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input {...register("password")} type="password" required />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <div className="flex flex-col w-full items-center">
+              <Button variant="default" className="w-full" type="submit">
+                Sign in
+              </Button>
+              <div className="border w-auto text-center text-sm m-4">
+                Don&apos;t have an account?{" "}
+                <Link href="/create-account" className="underline">
+                  Sign up
+                </Link>
+              </div>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
--- apps/web/module/Register/index.tsx
+++ apps/web/module/Register/index.tsx
@@ -9,127 +9,127 @@ import useRegister from "./hooks/useRegister";
 import { useRouter } from "next/navigation";
 
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/common/components/shadcn/ui/card";
 import { Input } from "@/common/components/shadcn/ui/input";
 import { Label } from "@/common/components/shadcn/ui/label";
 
 const Register = () => {
-	const router = useRouter();
-	const { register, handleSubmit } = useForm<T_UserRegister>();
-	const { mutate: registerUser } = useRegister();
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<T_UserRegister>();
+  const { mutate: registerUser } = useRegister();
 
-	const onSubmit = async (data: T_UserRegister) => {
-		try {
-			const callBackReq = {
-				onSuccess: (data: any) => {
-					if (!data.error) {
-						if (data.action?.data.action.link) {
-							router.push(data.action.link);
-						}
-					} else {
-						toast.error(String(data.message));
-					}
-				},
-				onError: (err: any) => {
-					toast.error(String(err));
-				},
-			};
-			registerUser(data, callBackReq);
-		} catch (error) {
-			console.error("Error adding user:", error);
-			toast.error("An error occurred. Please try again later.");
-		}
-	};
+  const onSubmit = async (data: T_UserRegister) => {
+    try {
+      const callBackReq = {
+        onSuccess: (data: any) => {
+          if (!data.error) {
+            if (data.action?.data.action.link) {
+              router.push(data.action.link);
+            }
+          } else {
+            toast.error(String(data.message));
+          }
+        },
+        onError: (err: any) => {
+          toast.error(String(err));
+        },
+      };
+      registerUser(data, callBackReq);
+    } catch (error) {
+      console.error("Error adding user:", error);
+      toast.error("An error occurred. Please try again later.");
+    }
+  };
 
-	return (
-		<form
-			onSubmit={handleSubmit(onSubmit)}
-			className="space-y-4"
-			action="#"
-			method="POST"
-		>
-			<div className="flex justify-center items-center h-screen">
-				<Card className="mx-auto max-w-sm">
-					<CardHeader>
-						<CardTitle className="text-xl">Sign Up</CardTitle>
-						<CardDescription>
-							Enter your information to create an account
-						</CardDescription>
-					</CardHeader>
-					<CardContent>
-						<div className="grid gap-4">
-							<div className="grid gap-2">
-								<Label htmlFor="clinicName">Clinic Name</Label>
-								<Input
-									id="clinicName"
-									type="clinicName"
-									{...register("clinicName", { required: true })}
-									placeholder=""
-									required
-								/>
-							</div>
-							<div className="grid grid-cols-2 gap-4">
-								<div className="grid gap-2">
-									<Label htmlFor="first-name">First name</Label>
-									<Input
-										id="firstName"
-										{...register("firstName", { required: true })}
-										placeholder=""
-										required
-									/>
-								</div>
-								<div className="grid gap-2">
-									<Label htmlFor="last-name">Last name</Label>
-									<Input
-										id="lastName"
-										{...register("lastName", { required: true })}
-										placeholder=""
-										required
-									/>
-								</div>
-							</div>
-							<div className="grid gap-2">
-								<Label htmlFor="email">Email</Label>
-								<Input
-									id="email"
-									type="email"
-									{...register("email", { required: true })}
-									placeholder=""
-									required
-								/>
-							</div>
-							<div className="grid gap-2">
-								<Label htmlFor="password">Password</Label>
-								<Input
-									id="password"
-									{...register("password", { required: true })}
-									type="password"
-									required
-								/>
-							</div>
-							<Button variant="default" type="submit" className="w-full">
-								Create an account
-							</Button>
-							<Button variant="outline" className="w-full">
-								Sign up with Google
-							</Button>
-						</div>
-						<div className="mt-4 text-center text-sm">
-							Already have an account?{" "}
-							<Link href="/" className="underline">
-								Sign in
-							</Link>
-						</div>
-					</CardContent>
-				</Card>
-			</div>
-		</form>
-	);
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="space-y-4"
+      action="#"
+      method="POST"
+    >
+      <div className="flex justify-center items-center h-screen">
+        <Card className="mx-auto max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-xl">Sign Up</CardTitle>
+            <CardDescription>
+              Enter your information to create an account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="clinicName">Clinic Name</Label>
+                <Input
+                  id="clinicName"
+                  type="clinicName"
+                  {...register("clinicName", { required: true })}
+                  placeholder=""
+                  required
+                />
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <div className="grid gap-2">
+                  <Label htmlFor="first-name">First name</Label>
+                  <Input
+                    id="firstName"
+                    {...register("firstName", { required: true })}
+                    placeholder=""
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="last-name">Last name</Label>
+                  <Input
+                    id="lastName"
+                    {...register("lastName", { required: true })}
+                    placeholder=""
+                    required
+                  />
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  {...register("email", { required: true })}
+                  placeholder=""
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  {...register("password", { required: true })}
+                  type="password"
+                  required
+                />
+              </div>
+              <Button variant="default" type="submit" className="w-full">
+                Create an account
+              </Button>
+              <Button variant="outline" className="w-full">
+                Sign up with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <Link href="/" className="underline">
+                Sign in
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </form>
+  );
 };
 
 export default Register;
